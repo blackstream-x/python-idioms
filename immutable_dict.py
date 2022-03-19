03111e511680d1472afa1e50b3f73d1734e968eb
blackstream-x <rainer@blackstream.de>
2022-03-19 14:25:45 +0100

    Bugfix and optimizations

--- immutable_dict.py
+++ immutable_dict.py
@@ -4,7 +4,7 @@
 
 immutable_dict
 
-immutable dict class
+An immutable and hashable dict-like class
 
 """
 
@@ -12,59 +12,67 @@ immutable dict class
 from collections import abc
 
 
-#
-# Classes
-#
-
-
 class ImmutableDict(abc.Mapping, abc.Hashable):
 
-    """Immutable dict-like class
+    """Immutable and hashable dict-like class
 
-    TODO: add .items() method
+    JSON or YAML serialization is not supported directly,
+    but can be done by creating a dict from the instance
+    (eg. using the .as_mutable_dict() method).
     """
 
     def __init__(self, *args, **kwargs):
         """Allocate the inner data structure"""
         data = dict(*args, **kwargs)
-        keylist = []
-        valuelist = []
-        self.__key_lookup = {}
-        for (index, (key, value)) in enumerate(data.items()):
-            if not isinstance(value, abc.Hashable):
-                raise ValueError("All values must be hashable!")
+        for value in data.values():
+            try:
+                hash(value)
+            except TypeError as error:
+                raise ValueError(
+                    f"Cannot create the {self.__class__.__name__} instance:"
+                    f" value {value!r} is not hashable!"
+                ) from error
             #
-            keylist.append(key)
-            valuelist.append(value)
-            self.__key_lookup[key] = index
         #
-        self.__keys = tuple(keylist)
-        self.__values = tuple(valuelist)
+        self.__keys = tuple(data.keys())
+        self.__values = tuple(data.values())
+
+    def as_mutable_dict(self):
+        """Return a standard dict from self"""
+        return dict(self)
+
+    def items(self):
+        """Return an iterator over (key, value) pairs"""
+        for index, key in enumerate(self.__keys):
+            yield key, self.__values[index]
+        #
 
     def __getitem__(self, name):
         """Return the value for key 'name'"""
-        index = self.__key_lookup[name]
-        return self.__values[index]
+        try:
+            return self.__values[self.__keys.index(name)]
+        except ValueError as error:
+            raise KeyError(name) from error
+        #
 
-    def __len__(self):
-        """Return the number of items"""
-        return len(self.__keys)
+    def __hash__(self):
+        """Return the hash of the str representation"""
+        return hash(repr(self))
 
     def __iter__(self):
         """Return an iterator over the keys"""
         return iter(self.__keys)
 
-    def __hash__(self):
-        """Return the hash of the str representation"""
-        return hash(str(self))
+    def __len__(self):
+        """Return the number of items"""
+        return len(self.__keys)
 
-    def __str__(self):
+    def __repr__(self):
         """String representation"""
         contents = ", ".join(
-            f"{key!r}: {self.__values[index]!r}"
-            for (index, key) in enumerate(self.__keys)
+            f"{key!r}: {value!r}" for key, value in self.items()
         )
-        return f"{self.__class__.__name__}({contents})"
+        return f"{self.__class__.__name__}({{{contents}}})"
 
 
 # vim: fileencoding=utf-8 sw=4 ts=4 sts=4 expandtab autoindent syntax=python:
