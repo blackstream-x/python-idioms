447d5dad704a7ae2a8634a4e0888498aa4ce251f
blackstream-x <rainer@blackstream.de>
2022-03-21 19:50:36 +0100

    Work on the deepfreeze() method

--- frozen_dict.py
+++ frozen_dict.py
@@ -55,10 +55,10 @@ class FrozenDict(abc.Mapping, abc.Hashable):
             raise KeyError(name) from error
         #
 
-    def get(self, name, default=None):
+    def get(self, key, default=None):
         """Return the value for key 'name' or default"""
         try:
-            return self[name]
+            return self[key]
         except KeyError:
             return default
         #
@@ -81,6 +81,40 @@ class FrozenDict(abc.Mapping, abc.Hashable):
             f"{key!r}: {value!r}" for key, value in self.items()
         )
         return f"{self.__class__.__name__}({{{contents}}})"
+
+
+def deepfreeze(item):
+    """Return a frozen (i.e. immutable and hashable)
+    pendant of item. For collections, this implies
+    that all members are frozen too.
+    """
+    if isinstance(item, (str, int, float, frozenset, FrozenDict)):
+        return item
+    #
+    if isinstance(item, set):
+        return frozenset(item)
+    #
+    if isinstance(item, (list, tuple)):
+        return tuple(deepfreeze(member) for member in item)
+    #
+    if isinstance(item, dict):
+        return FrozenDict(
+            (key, deepfreeze(value)) for (key, value) in item.items()
+        )
+    #
+    # Try hashing the item
+    hash(item)
+    #
+    # Is the item an iterable?
+    # If not -> assume it is frozen.
+    # This is not 100 % safe...
+    try:
+        None in item
+    except TypeError:
+        return item
+    #
+    # TODO: check if item is a collection, mutable etc.
+    raise NotImplementedError
 
 
 # vim: fileencoding=utf-8 sw=4 ts=4 sts=4 expandtab autoindent syntax=python:
